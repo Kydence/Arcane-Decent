de355362b4b4863fba3856a989c2788a0c0595a7
Zach Puderbaugh <zach.puderbaugh@gmail.com>
2025-10-10 21:39:55 -0400

    playermovementscript and some other fixes

---
+++ Arcane_Decent/Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D body;
+    void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocity.y);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
+        }
+    }
+}
