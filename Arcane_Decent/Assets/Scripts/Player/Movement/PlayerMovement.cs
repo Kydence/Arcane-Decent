634d592eb922952891c84bdd1a1279aa441c4bce
Zach Puderbaugh <zach.puderbaugh@gmail.com>
2025-10-13 19:59:45 -0400

    Added some jump stuff and tested out animator

--- Arcane_Decent/Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Arcane_Decent/Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -4,18 +4,54 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     public Rigidbody2D body;
+    Animator anim;
+    bool grounded;
+
     void Awake()
     {
+        // grab references for rigidbody and animator fromm object
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
 
-        if (Input.GetKey(KeyCode.Space))
+        // flip player when moving left
+        if (horizontalInput > 0.01f)
         {
-            body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
+            transform.localScale = new Vector3(2, 2, 2);
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-2, 2, 2);
+        }
+
+        // jump
+        if (Input.GetKey(KeyCode.Space) && grounded)
+        {
+            Jump();
+        }
+
+        // set animator parameters
+        anim.SetBool("IsWalk", horizontalInput != 0);
+        // anim.SetTrigger("IsFalling");
+        // anim.SetBool("IsJump", grounded);
+    }
+
+    private void Jump()
+    {
+        body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
+        grounded = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
         }
     }
 }
