96f424bdfe4f12d5c3edd9ad855c7838f0d573ce
Kydence <clawsoncaden3@gmail.com>
2025-11-14 01:54:36 -0500

    credits

--- Arcane_Decent/Assets/Scripts/Core/CameraController.cs
+++ Arcane_Decent/Assets/Scripts/Core/CameraController.cs
@@ -4,9 +4,11 @@ public class CameraController : MonoBehaviour
 {
     [Header("Follow")]
     public Transform player;
-
+    [SerializeField] private float disY;
+    [SerializeField] private float disX;
+    
     void Update()
     {
-        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        transform.position = new Vector3(player.position.x+disX, player.position.y+disY, transform.position.z);
     }
 }
