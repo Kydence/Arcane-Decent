803fc98e676e6257db53268e6364e7053c678ffd
Zach Puderbaugh <zach.puderbaugh@gmail.com>
2025-11-14 11:23:26 -0500

    added health pickup sfx

--- Arcane_Decent/Assets/Scripts/Health/HealthCollectible.cs
+++ Arcane_Decent/Assets/Scripts/Health/HealthCollectible.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class HealthCollectible : MonoBehaviour
 {
     [SerializeField] private float healthValue;
+    [SerializeField] private AudioClip healthPickupSound;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
+            CloseSoundManager.instance.PlaySound(healthPickupSound);
             collision.GetComponent<Health>().AddHealth(healthValue);
             gameObject.SetActive(false);
         }
