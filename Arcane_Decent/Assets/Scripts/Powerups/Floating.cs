885af7e6d93ad5b9ad26ac635a10f08e0141528c
Zach Puderbaugh <zach.puderbaugh@gmail.com>
2025-11-17 15:08:05 -0500

    ugh
    -had to reimplement the floating script cuz it got deleted for some reason
    -edited the color of the slime ball
    -started to take away the i frames for enemies since they dont need them and they change colors for some reason with them

---
+++ Arcane_Decent/Assets/Scripts/Powerups/Floating.cs
@@ -0,0 +1,21 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Floating : MonoBehaviour
+{
+    [Header("Floating Setting")]
+    public float amplitude = 0.25f;
+    public float frequency = 2f;
+    Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+    void Update()
+    {
+        float newY = startPos.y + Mathf.Sin(Time.time * frequency) * amplitude;
+        transform.position = new Vector3(startPos.x, newY, startPos.z);
+    }
+}
