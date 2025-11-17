5825819893cd346f2a423a9d9f3f759b4e0a42a6
Kydence <clawsoncaden3@gmail.com>
2025-11-16 20:09:01 -0500

    try boss stuff

---
+++ Arcane_Decent/Assets/Scripts/Enemy/BossPatrol.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+
+public class BossPatrol : MonoBehaviour
+{
+
+    [Header("Patrol Points")]
+    [SerializeField] private Transform leftEdge;
+    [SerializeField] private Transform rightEdge;
+
+    [Header("Enemy")]
+
+    [SerializeField] private Transform enemy;
+
+    [Header("movement parameters")]
+    [SerializeField] private float speed;
+    private Vector3 initScale;
+    private bool movingLeft;
+    [SerializeField] private float idleDuration;
+    private float idleTimer;
+    
+   [SerializeField] private Animator anim;
+
+    [Header("boss specific stuff")]
+   [SerializeField] private Transform player;
+
+    private void Awake()
+    {
+        initScale = enemy.localScale;
+       
+    }
+
+    void Update()
+    {
+        if (movingLeft)
+        {
+            if ( enemy.position.x>= player.position.x)
+                MoveInDirection(-1);
+            else
+            {
+                //change direciton
+                DirectionChange();
+            }
+        }
+        else
+        {
+            if (enemy.position.x<= player.position.x)
+                MoveInDirection(1);
+            else
+            {
+                //change direction
+                DirectionChange();
+            }
+        }   
+    }
+
+    private void DirectionChange()
+    {
+        anim.SetBool("isWalk",false);
+        idleTimer += Time.deltaTime;
+        if (idleTimer > idleDuration)
+        {
+           
+            movingLeft = !movingLeft;
+        }
+    }
+
+    private void MoveInDirection(int _direction)
+    {
+        anim.SetBool("isWalk",true);
+        idleTimer = 0;
+        //Make Enemy face direction
+        enemy.localScale = new Vector2(Mathf.Abs(initScale.x) * _direction, initScale.y);
+        //Move in that direciton
+        enemy.position = new Vector3(enemy.position.x + Time.deltaTime * _direction * speed, enemy.position.y, enemy.position.z);
+    }
+
+ 
+
+}
