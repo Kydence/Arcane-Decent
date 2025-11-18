0646bbc78cc3e83844d1b1dfc6ca9df665ad4e8e
Zach Puderbaugh <zach.puderbaugh@gmail.com>
2025-11-17 20:06:31 -0500

    made boss music start and stop upon entering and exiting boss room

--- Arcane_Decent/Assets/Scripts/Core/Camera/BossRoomTrigger.cs
+++ Arcane_Decent/Assets/Scripts/Core/Camera/BossRoomTrigger.cs
@@ -1,14 +1,21 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BossRoomTrigger : MonoBehaviour
 {
     public CameraController cameraController;
+    public AudioSource bossMusic;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             cameraController.EnterBossRoom();
+
+            if (bossMusic != null)
+            {
+                bossMusic.Play();
+            }
         }
     }
 
@@ -17,6 +24,11 @@ public class BossRoomTrigger : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             cameraController.ExitBossRoom();
+
+            if (bossMusic != null)
+            {
+                bossMusic.Stop();
+            }
         }
     }
 }
